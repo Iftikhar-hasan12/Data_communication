69e4c6ad53bc869a0792b798c1af17f3f83ca851
Iftikhar-hasan12 <iftikharhasan04@gmail.com>
2025-05-17 13:43:45 +0600

    Update IPv4_Binary_to_Decimal.c

--- Lab_manual_02/IPv4_Binary_to_Decimal.c
+++ Lab_manual_02/IPv4_Binary_to_Decimal.c
@@ -1,74 +1,41 @@
-
 #include<stdio.h>
-#include<string.h>
-char* ToBin(char ch[]);
+
+char * ToBin(char ip[]);
 int main()
 {
-   // char ip[100];
-    printf("Enter : ");
-   // scanf("% [^\n]",ip);
-     //char ip[40]="11.22.33.4";
-     char ip[40]="11.10.001.0";
-    char*token = strtok(ip, ".");
+
+    char ip[100];
+    printf("Enter Binary digit : ");
+    scanf(" %[^\n]", ip);
+
+    char *token = strtok(ip, ".");
 
     while(token!=NULL)
     {
-        printf("%d ",ToDec(token));
 
-        token = strtok(NULL, ".");
+        printf(" %d",ToDec(token));
+        token = strtok(NULL,".");
+
     }
+
 
 }
 
-int ToDec(char ch[])
+int ToDec(char ip[])
 {
-    int n = atoi(ch);
-    int len=strlen(ch);
-    int base =1;
-    int dec =0;
+    int bin = atoi(ip);
+    int rem, base =1, dec =0;
 
-    for(int i=len-1; i>=0 ; i--)
+    for(int i = bin; i!=0; i=i/10)
     {
-        if(ch[i]=='1')
-        {
-            dec = base + dec;
-        }
+        rem = i%10;
+        dec = dec + (rem * base );
         base = base *2;
 
     }
+
     return dec;
 
-}
-
-
-
-
-
-
-
-char* ToBin(char ch[])
-{
-   static char bin[9];
-    for(int i=0; i<8; i++)
-    {
-        bin[i]='0';
-    }
-
-    int n = atoi(ch);
-    int rem ;
-    int k=7;
-    for(int i=n; i!=0; i=i/2)
-    {
-        rem = i%2;
-        bin[k]=rem+'0';
-        k--;
-
-    }
-bin[8]='\0';
-
-
-    return bin;
-
-
 
 }
+
